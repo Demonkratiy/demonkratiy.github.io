ebd9db6caa1ffefed6d9996ceb18d850f920c808
Aleksei Semenov <aleksei.semenov@akvelon.com>
2020-02-25 17:21:20 +0300

    Updates

---
+++ precache-manifest.2f9de431d9d4c2c248c9666a87e6f4be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e743a6a132365e527d5a762451f2bf",
+    "url": "/Demonkratiy/demonkratiy.github.io.git/index.html"
+  },
+  {
+    "revision": "84b380bfd81bc4b00f75",
+    "url": "/Demonkratiy/demonkratiy.github.io.git/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "25667025e8b4e01233fa",
+    "url": "/Demonkratiy/demonkratiy.github.io.git/static/js/2.a37675e3.chunk.js"
+  },
+  {
+    "revision": "ea8761bfdc77ba82ac0139b0e0ab83b5",
+    "url": "/Demonkratiy/demonkratiy.github.io.git/static/js/2.a37675e3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "84b380bfd81bc4b00f75",
+    "url": "/Demonkratiy/demonkratiy.github.io.git/static/js/main.730d823b.chunk.js"
+  },
+  {
+    "revision": "53449e207b50cea18b5b",
+    "url": "/Demonkratiy/demonkratiy.github.io.git/static/js/runtime-main.7f0c5f32.js"
+  }
+]);
